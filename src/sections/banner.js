d81c2a897ac311bba036fd9aeb6f1a74df8346b0
zlatan010 <36987079+zlatan010@users.noreply.github.com>
2022-11-27 05:17:42 +0100

    Update banner.js

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -38,7 +38,7 @@ export default function Banner() {
           <div className={`${styles.buttons}`}>
             <div>
               <button className={`${styles.but}`}>
-                <a target="_blank" href="/pdf/resume_francisco_pantoja.pdf">
+                <a target="_blank" href="/pdf/Oussama_Mouchrit_Devops.pdf">
                   DOWNLOAD RESUME
                 </a>
               </button>
